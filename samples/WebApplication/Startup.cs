58ed6c299c921b9784ae32245a891d840b87e53d
Vladimir Panchenko <dropsonic.pan@gmail.com>
2021-08-01 12:40:28 +0300

    Tabs->spaces

--- samples/WebApplication/Startup.cs
+++ samples/WebApplication/Startup.cs
@@ -7,11 +7,11 @@ using WebApplication.Services;
 
 namespace WebApplication
 {
-	public class Startup
-	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
             services
                 .AddMvc()
@@ -22,20 +22,20 @@ namespace WebApplication
             services.AddControllers();
         }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseRouting();
             
-			app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-		}
-	}
+        }
+    }
 }
